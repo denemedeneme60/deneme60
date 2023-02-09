6fed7b21883fa3b4ad2586da2e4979fed885baeb
Yağız Sarıdoğan <yagizsaridogan@gmail.com>
2023-02-09 20:49:15 +0300

    istanbul

--- JavaOgreniyorum.java
+++ JavaOgreniyorum.java
@@ -6,7 +6,7 @@ public class JavaOgreniyorum {
     public static void main(String[] args) {
         System.out.println("Selamlar Türkiyem");
         System.out.println("Selamlar Tokatim");
-        System.out.println("selamlar ankaram");
+        System.out.println("selamlar istanbul");
 
         
     }
