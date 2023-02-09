788b13a3596a1f4be51931b46ed2cc095d71b20f
FurkanOzek <fur_ozek@hotmail.com>
2023-02-09 20:52:27 +0300

    ...

--- JavaOgreniyorum.java
+++ JavaOgreniyorum.java
@@ -6,12 +6,11 @@ public class JavaOgreniyorum {
     public static void main(String[] args) {
         System.out.println("Selamlar Türkiyem");
         System.out.println("Selamlar Tokatim");
-<<<<<<< HEAD
+
         System.out.println("Oldu mu beyler");
-=======
+
         System.out.println("selamlar istanbul");
 
->>>>>>> main
         
     }
 }
