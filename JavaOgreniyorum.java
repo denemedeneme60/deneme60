1ab0f3f83c8bad8c1c83c42abeb9585542c980b3
Ahmet Burak KILIÇKAYA <a.burakilickaya@gmail.com>
2023-02-09 20:47:05 +0300

    denedim

--- JavaOgreniyorum.java
+++ JavaOgreniyorum.java
@@ -6,6 +6,7 @@ public class JavaOgreniyorum {
     public static void main(String[] args) {
         System.out.println("Selamlar Türkiyem");
         System.out.println("Selamlar Tokatim");
+        System.out.println("Oldu mu beyler");
         
     }
 }
