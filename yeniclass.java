abdf33dd6e634a726c506971e8c5d87200753d1e
FurkanOzek <fur_ozek@hotmail.com>
2023-02-09 21:17:25 +0300

    furkan değişikliği 2

--- yeniclass.java
+++ yeniclass.java
@@ -1,9 +1,15 @@
 public class yeniclass {
 
+    
+
     public void classmubarak()
     {
     System.out.println("Yeni class denemesi");
     System.out.println("Kargasa cikmaz ins");
+    }
+
+    public void furkanMethod(){
+        System.out.println("furkan özek 3169");
     }
 
 }
