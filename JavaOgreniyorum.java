54375a1c5a3a7904a0cffec285f5b7e9d31683d6
Yağız Sarıdoğan <yagizsaridogan@gmail.com>
2023-02-09 20:48:13 +0300

    Auto stash before merge of "main" and "origin/main"

--- JavaOgreniyorum.java
+++ JavaOgreniyorum.java
@@ -6,6 +6,8 @@ public class JavaOgreniyorum {
     public static void main(String[] args) {
         System.out.println("Selamlar Türkiyem");
         System.out.println("Selamlar Tokatim");
+        System.out.println("selamlar ankaram");
+
         
     }
 }
